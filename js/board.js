d58a5e4306115323211d81aabc4614d5c556ca9a
halsam <wchauks@gmail.com>
2020-12-23 21:59:58 -0500

    finding combos in rows

--- js/board.js
+++ js/board.js
@@ -60,6 +60,105 @@ class Board {
         console.log("Board Cleared");
     }
 
+    // rows start with 0, 6, 12, 18, 24 and increment by 1 on each
+    // returns the orb numbers in the row
+    getRow(row) {
+        var orbs = [];
+        var i = 0;
+        while(i < this.column) {
+            orbs.push(row*6 + i);
+            i++
+        }
+        return orbs;
+    }
+
+    // columns start with 0, 1, 2, 3, 4, 5 and increment by 6 on each
+    // returns the orb numbers in the column
+    getColumn(column) {
+        var orbs = [];
+        for(var i = column; i < this.numOrbs; i += 6) {
+            orbs.push(i);
+        }
+        return orbs;
+    }
+
+    //receives a list of orbs (in row and column) to check if there are any matches
+    //adds any combos found to the remove list
+    getMatches(removeList, uncheckedListOfOrbs) {
+
+    }
+
+    //receives a list of orb numbers that make up the combo and adds it to the remove list
+    addComboToRemoveList(removeList, comboOrbNums) {
+        for(var tmp = 0; tmp < comboOrbNums.length; tmp++) {
+            removeList.push(comboOrbNums[tmp]);
+        }
+    }
+
+    //calls getMatches on each row and column and stores any combos to the remove list
+    //updates matchFound
+    checkForMatch() {
+        var matchFound = false;
+        var minNumForMatch = 3;     //minimum number to be considered a match
+        var orbsForRemoval = [];    //orb positions of combos found so that they can be removed
+        
+        for(var i = 0; i < this.row; i++) {
+            var comboColor, nextOrbColor;
+            var currentCombo = [];
+            var row = this.getRow(i);
+            // console.log(row);
+            for(var j=0; j<row.length; j++) {
+                if(currentCombo.length == 0) {
+                    comboColor = this.square[row[j]].orb.getColor();
+                    currentCombo.push(row[j]);
+                }
+                else {
+                    nextOrbColor = this.square[row[j]].orb.getColor();
+                    if(isMatch(comboColor, nextOrbColor)) {
+                        currentCombo.push(row[j]);
+                        if(j==row.length-1 && currentCombo.length >= minNumForMatch) {
+                            this.addComboToRemoveList(orbsForRemoval, currentCombo);
+                        }
+                    }
+                    else {
+                        if(currentCombo.length >= minNumForMatch) {
+                            console.log("combo logged");
+                            this.addComboToRemoveList(orbsForRemoval, currentCombo);
+                        }
+                        comboColor = this.square[row[j]].orb.getColor();
+                        currentCombo = [];
+                        currentCombo.push(row[j]);
+                    }
+                }
+            }
+        }
+
+        for(var i = 0; i < this.column; i++) {
+            var column = this.getColumn(i);
+            // console.log(column);
+        }
+
+        console.log(orbsForRemoval);
+
+        this.updateHasMatches(matchFound);
+        return matchFound;
+    }
+
+    // removeMatches(x) {
+    //     console.log(x);
+    // }
+
+    updateHasMatches(matchFound) {
+        if (matchFound == false) this.hasMatches = false;
+        else this.hasMatches = true;
+    }
+
+    // drop() {
+    //     do {
+    //         this.removeMatches(this.checkForMatch());
+    //     } while(this.hasMatches == true);
+    // }
+
     updateAfterMove() {
         var tmp;
         var boardStr = "";
--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,8 @@ function printBoard(){
 function printBoardStr(tmp) {
     console.log("Board:"+"\n\t"+tmp.substr(0,6)+"\n\t"+tmp.substr(6,6)+"\n\t"+tmp.substr(12,6)+"\n\t"+tmp.substr(18,6)+"\n\t"+tmp.substr(24,6));
 }
+
+function isMatch(orb1, orb2) {
+    if(orb1 == orb2) return true;
+    else return false;
+}
